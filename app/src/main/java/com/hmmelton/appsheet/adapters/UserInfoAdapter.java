399bff13636290e57ca000beef065f39bc6a9ea8
Harrison Melton <hmmelton@comcast.net>
2017-04-19 10:51:30 -0700

    created adapter for RecyclerView

---
+++ app/src/main/java/com/hmmelton/appsheet/adapters/UserInfoAdapter.java
@@ -0,0 +1,10 @@
+package com.hmmelton.appsheet.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by harrison on 4/19/17.
+ */
+
+public class UserInfoAdapter extends RecyclerView.Adapter {
+}
