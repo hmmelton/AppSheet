bb73e735f324ce9e87ab7345da2ee2287c7333a7
Harrison Melton <hmmelton@comcast.net>
2017-04-19 00:46:54 -0700

    finished new callback

--- app/src/main/java/com/hmmelton/appsheet/interfaces/GetUserIdsCallback.java
+++ app/src/main/java/com/hmmelton/appsheet/interfaces/GetUserIdsCallback.java
@@ -1,8 +1,12 @@
 package com.hmmelton.appsheet.interfaces;
 
+import java.util.List;
+
 /**
  * Created by harrison on 4/18/17.
+ * This interface is a callback used for fetching user ID's from the AppSheet web service.
  */
 
 public interface GetUserIdsCallback {
+    void onComplete(List<Integer> userIds);
 }
